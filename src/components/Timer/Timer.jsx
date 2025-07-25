15825ce9cc3ebdd750a210aad62026da813b3770
Przemylaw Koczwara <134004284+przemekkoczwara@users.noreply.github.com>
2025-07-25 22:23:01 +0200

    Added reset props to component Timer

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import styles from './Timer.module.css';
 
-export default function Timer({ seconds, timeToEnd, isActive }) {
+export default function Timer({ seconds, timeToEnd, isActive, resetTimer }) {
   const [timeLeft, setTimeLeft] = useState(seconds);
+  useEffect(() => {
+    setTimeLeft(seconds);
+  }, [resetTimer, seconds]);
 
   useEffect(() => {
     if (!isActive) return;
+
     if (timeLeft === 0) {
       timeToEnd();
       return;
@@ -15,13 +20,13 @@ export default function Timer({ seconds, timeToEnd, isActive }) {
     }, 1000);
 
     return () => clearInterval(timerId);
-  }, [isActive,timeLeft, timeToEnd]);
+  }, [isActive, timeLeft, timeToEnd]);
 
   const min = Math.floor(timeLeft / 60);
   const sec = Math.floor(timeLeft % 60);
 
   return (
-    <div>
+    <div className={styles.timer}>
       {min.toString().padStart(2, '0')}:{sec.toString().padStart(2, '0')}
     </div>
   );
