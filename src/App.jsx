930f4b26cf3607500cc6e3a092c3d3787bc9515a
Przemylaw Koczwara <134004284+przemekkoczwara@users.noreply.github.com>
2025-08-03 22:15:27 +0200

    Deleted app.css -> file not used && added to app new source Route > history

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Amrap from './components/Amrap/Amrap';
-import Wod from './components/Wod/Wod';
-import Emom from './components/Emom/Emom';
+// import Wod from './components/Wod/Wod';
+// import Emom from './components/Emom/Emom';
+import History from './components/History/History';
 
 export default function App() {
   return (
@@ -11,10 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/amrap" element={<Amrap />} />
-        <Route path="/wod" element={<Wod />} />
-        <Route path="/amrap" element={<Emom />} />
-        {/* <Route path="/summary" element={<Summary />} />
-        <Route path="/history" element={<History />} />  */}
+        <Route path="/history" element={<History />} />
       </Routes>
     </BrowserRouter>
   );
