77968d149b5a2b9f4cf18cc601560ffcfd9bffcb
Przemylaw Koczwara <134004284+przemekkoczwara@users.noreply.github.com>
2025-08-04 20:57:18 +0200

    Modyfied WOD component -> new style & structure

--- src/components/Wod/Wod.jsx
+++ src/components/Wod/Wod.jsx
@@ -1,83 +1,186 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Wod.module.css';
 import Button from '../Button/Button';
 import cardImage from '../../assets/Wod2.jpg';
-import EXERCISES from '../../data/exercises';
 import getRandomExercises from '../../utils/workoutGenerator';
 import { useNavigate } from 'react-router-dom';
 import TimerUp from '../TimerUp/TimerUp';
+import saveScoreHistory from '../../utils/saveHistory';
+// import saveScoreHistory from '../../utils/saveHistory';
 
 export default function Wod() {
   const navigate = useNavigate();
   const backToHome = () => navigate('/');
 
+  // useState
+
+  //exercises
+  const [allExercises, setAllExercises] = useState([]);
   const [workout, setWorkout] = useState(null);
+  // timer
   const [isTimerOn, setIsTimerOn] = useState(false);
   const [hasStartedTimer, setHasStartedTimer] = useState(false);
   const [resetTimer, setResetTimer] = useState(0);
-  const [timerSeconds] = useState(0);
-  const [isFinished, setIsFinished] = useState(false);
 
+  // user controls
+  const [selectedLevel, setSelectedLevel] = useState(null);
+  const [exerciseCount, setExerciseCount] = useState(null);
+  const [targetRounds, setTargetRounds] = useState(5);
+  const [timerSeconds, setTimerSeconds] = useState(0);
+  const [isWorkoutStopped, setIsWorkoutStopped] = useState(false);
+  // for hidden controls
+  const [isWorkoutGenerated, setIsWorkoutGenerated] = useState(false);
+
+  //save score
+
+  const [isWorkoutFinished, setIsWorkoutFinished] = useState(false);
+  const [roundsCompleted, setRoundsCompleted] = useState(0);
+
+  // useEffect
+
+  useEffect(() => {
+    fetch('/database.json')
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        return response.json();
+      })
+      .then((data) => setAllExercises(data.exercises))
+      .catch((error) => console.error('Fetch error', error));
+  }, []);
+
+  // select workout
   const randomWorkout = () => {
-    const selectedExercises = getRandomExercises(EXERCISES, 5);
+    if (allExercises === 0 || !exerciseCount) return;
+
+    const selectedExercises = getRandomExercises(allExercises, exerciseCount);
     setWorkout({ exercises: selectedExercises });
     setHasStartedTimer(true);
     setIsTimerOn(false);
-    setResetTimer((prev) => prev - 1);
+    setResetTimer((prev) => prev + 1);
+    setIsWorkoutGenerated(true);
   };
 
   // start wod
   const workoutStart = () => {
     if (!workout) return;
 
-    if (isTimerOn) {
-      setIsTimerOn(false);
-      setIsFinished(true);
+    if (!isTimerOn && !isWorkoutStopped && !isWorkoutFinished) {
+      setIsTimerOn(true);
       return;
     }
 
-    if (isFinished) {
-      alert('Your score will be saved');
-      setIsFinished(false);
-      setResetTimer((prev) => prev + 1);
+    if (isTimerOn && !isWorkoutStopped) {
+      setIsTimerOn(false);
+      setIsWorkoutStopped(true);
+      setIsWorkoutFinished(true);
       return;
     }
-    setIsTimerOn(true);
-    hasStartedTimer(true);
-    console.log('Star WOD workout');
+    if (isWorkoutStopped && isWorkoutFinished) {
+      saveScoreHistory();
+    }
   };
 
   return (
     <section className={styles.container}>
+      <div className={styles.backButtonWrapper}>
+        <Button className={styles.backToHome} onClick={backToHome}>
+          <span
+            className={`${styles.materialSymbols} material-symbols-outlined`}
+          >
+            keyboard_double_arrow_left
+          </span>
+        </Button>
+      </div>
       <div className={styles.group}>
         <div className={styles.img}>
-          <img src={cardImage} alt="Women sitting on the ball" />
+          <img src={cardImage} alt="Ring row men" />
         </div>
         <div className={styles.workoutContext}>
           <h2 className={styles.title}>WOD</h2>
-          <p className={styles.descrpition}>
-            Classic Workout of the Day – a mix of functional exercises with
-            reps/time goals.
-          </p>
-          <nav>
-            <div className={styles.navigateButtons}>
-              <Button styleType="back" onClick={backToHome}>
-                Go Back
-              </Button>
-              <Button styleType="random" onClick={randomWorkout}>
-                Random Training
+          {!isWorkoutGenerated && (
+            <p className={styles.description}>
+              Classic Workout of the Day – a mix of functional exercises with
+              reps/time goals.
+            </p>
+          )}
+          {!isWorkoutGenerated && (
+            <div className={styles.workoutControlsWrapper}>
+              <div className={styles.workoutControls}>
+                <p>Choose your level</p>
+                {['easy', 'medium', 'hard'].map((level) => (
+                  <Button
+                    key={level}
+                    active={selectedLevel === level}
+                    onClick={() => setSelectedLevel(level)}
+                  >
+                    {level}
+                  </Button>
+                ))}
+              </div>
+              <div className={styles.workoutControls}>
+                <p>Number of exercises</p>
+                {[3, 4, 5, 6, 7].map((count) => (
+                  <Button
+                    key={count}
+                    active={exerciseCount === count}
+                    onClick={() => setExerciseCount(count)}
+                  >
+                    {count}
+                  </Button>
+                ))}
+              </div>
+              <div className={styles.workoutControls}>
+                <p>Set number of rounds</p>
+                <div className={styles.roundsWrapper}>
+                  <Button
+                    onClick={() =>
+                      setTargetRounds((prev) => Math.max(prev - 1, 1))
+                    }
+                  >
+                    -
+                  </Button>
+                  <span>{targetRounds} Rounds</span>
+                  <Button
+                    onClick={() =>
+                      setTargetRounds((prev) => Math.min(prev + 1, 10))
+                    }
+                  >
+                    +
+                  </Button>
+                </div>
+              </div>
+            </div>
+          )}
+          {selectedLevel && exerciseCount && !isWorkoutFinished && (
+            <div className={styles.randomButtonWrapper}>
+              <Button
+                className={styles.buttonAmrap}
+                onClick={randomWorkout}
+                disabled={allExercises.length === 0}
+              >
+                SELECT WOD
               </Button>
             </div>
-          </nav>
-          {hasStartedTimer && (
+          )}
+          {hasStartedTimer && !isWorkoutFinished && (
             <TimerUp
               seconds={timerSeconds}
               isActive={isTimerOn}
               resetTimer={resetTimer}
             />
           )}
-          <div className={styles.workoutList}>
-            {workout && (
+          {hasStartedTimer && !isWorkoutFinished && (
+            <p className={styles.targetInfo}>
+              Target: <span>{targetRounds} </span>ROUNDS
+            </p>
+          )}
+          <div
+            className={`${styles.workoutList} ${
+              isWorkoutFinished ? styles.hidden : ''
+            }`}
+          >
+            {workout && !isWorkoutFinished && (
               <ul>
                 {workout.exercises.map((exercise, index) => (
                   <li key={index}>
@@ -87,16 +190,34 @@ export default function Wod() {
               </ul>
             )}
           </div>
-          {workout && (
-            <Button
-              styleType={isTimerOn ? 'stop' : isFinished ? 'save' : 'start'}
-              onClick={workoutStart}
-            >
-              {isTimerOn ? 'STOP' : isFinished ? 'SAVE SCORE' : 'Start WOD'}
+          {isWorkoutFinished && (
+            <div className={styles.roundInputWrapper}>
+              <p className={styles.successMsg}>
+                Fantastic work! You're amazing!!
+              </p>
+              <p className={styles.roundDescrption}>How many rounds you did?</p>
+              <div className={styles.roundControls}>
+                <Button
+                  onClick={() =>
+                    setRoundsCompleted((prev) => Math.max(prev - 1, 0))
+                  }
+                >
+                  -
+                </Button>
+                <span className={styles.roundValue}>{roundsCompleted}</span>
+                <Button onClick={() => setRoundsCompleted((prev) => prev + 1)}>
+                  +
+                </Button>
+              </div>
+            </div>
+          )}
+          {workout && !isTimerOn && (
+            <Button className={styles.workoutButtonStart} onClick={workoutStart}>
+              cos tu nie działa - reszta jutro
             </Button>
           )}
         </div>
       </div>
     </section>
-  ); 
+  );
 }
