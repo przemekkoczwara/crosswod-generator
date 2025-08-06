16bd4265e147a3cfc4b0000d330db018da81a9d3
Przemylaw Koczwara <134004284+przemekkoczwara@users.noreply.github.com>
2025-08-06 22:41:43 +0200

    Modyfied -> added new Route, history

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Amrap from './components/Amrap/Amrap';
-// import Wod from './components/Wod/Wod';
-// import Emom from './components/Emom/Emom';
+import Wod from './components/Wod/Wod';
+import Emom from './components/Emom/Emom';
 import History from './components/History/History';
 
 export default function App() {
@@ -12,6 +12,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/amrap" element={<Amrap />} />
+        <Route path="/wod" element={<Wod />} />
+        <Route path="/emom" element={<Emom />} />
         <Route path="/history" element={<History />} />
       </Routes>
     </BrowserRouter>
